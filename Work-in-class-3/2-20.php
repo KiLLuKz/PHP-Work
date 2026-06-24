de08454b3a195ceb3114f6903be616a7083cad20
KiLLuKz <quemaster123@gmail.com>
2026-06-24 15:02:03 +0700

    Create 2-20.php

---
+++ Work-in-class-3/2-20.php
@@ -0,0 +1,14 @@
+<?php
+$number = 2;
+echo "แสดงเลขคู่ตั้งแต่ 2 ถึง 20 <br>";
+echo "--------------------- <br>";
+
+do {
+    echo $number . " <br>";
+    
+    $number += 2;
+    
+} while ($number <= 20);
+
+echo "<br>";
+?>
